a49112584d82219b3b678ad474f05d46adae53c8
Aamir Abbas <aamir@ogaamyr.com>
2025-02-23 13:43:57 +0000

    styling setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import "./App.css"
+import "./styles.css"
 
 function App() {
 
   return (
     <div>
-      Clean Slate
+      <ul id="list">
+        <li className="list-item">
+          <label className="list-item-label">
+            <input type="checkbox" data-list-item-checkbox />
+            <span data-list-item-text>Item 1</span>
+          </label>
+          <button data-button-delete>Delete</button>
+        </li>
+        <li className="list-item">
+          <label className="list-item-label">
+            <input type="checkbox" checked data-list-item-checkbox />
+            <span data-list-item-text>Item 2</span>
+          </label>
+          <button data-button-delete>Delete</button>
+        </li>
+      </ul>
+
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input type="text" id="todo-input" />
+        <button>Add Todo</button>
+      </div>
     </div>
   );
 }
