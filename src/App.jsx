5249d591eb3328cf9aa3a6f3bfd317548f8232c3
Aamir Abbas <aamir@ogaamyr.com>
2025-02-23 14:36:24 +0000

    set up state tasksArray to manage tasks in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import "./styles.css"
+import { v4 as uuid_v4 } from 'uuid';
+import { useState } from 'react';
+
+
 import { Form } from './Form'
 import { ListItem } from './ListItem'
 
+import "./styles.css"
+
 function App() {
+
+  const [tasksArray, setTasksArray] = useState([{
+    text: "Task 1 - NOT Complete",
+    completed: false
+  },
+  {
+    text: "Task 2 - Complete",
+    completed: true
+  }])
 
   return (
     <div>
       <ul id="list">
-        <ListItem checkedStatus={false}>Checked: FALSE</ListItem>
-        <ListItem checkedStatus={true}>Checked: TRUE</ListItem>
+        {tasksArray.map(task => {
+          return (
+            <ListItem
+              key={uuid_v4()}
+              checkedStatus={task.completed}>
+              {task.text}
+            </ListItem>);
+        })}
       </ul>
 
       <Form></Form>
